fa4ada9565d1ca4b4e0dbbe7c20e3eac1e5085d9
Aman Khare <khareaman754@gmail.com>
2025-06-08 13:05:01 +0530

    feat: Adding all files first time

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+console.log(`\nAman Khare - Full Stack Developer`);
+console.log(`---------------------------------------`);
+console.log(`💼 LinkedIn: + https://www.linkedin.com/in/khareaman/`);
+console.log(
+  `👩🏻‍💻 Skills: + (MERN Stack) | Frontend: HTML, CSS, Javascript, React | Backend: Node.js, Express.js, MongoDB, Postman`
+);
+console.log(`📌 Location: + Bhiwadi, India`);
+console.log(`📧 Email: khareaman754@gmail.com`);
+console.log(`📂 GitHub: https://github.com/Amkr1997`);
+console.log(
+  `Education: + B.Tech in Electrical and Electronics Engineering from OIST Bhopal`
+);
+console.log(`Projects: -----------------------------------------------`);
+console.log(
+  `1. + Pustaks: https://pustaks.vercel.app/ + {An online book store}`
+);
+console.log(`2. + Sociogram: https://sociog.vercel.app/ + {Social Media}`);
+console.log(`😊 Thank you for checking out my resume. I hope you liked it.`);
+console.log(`---------------------------------------`);
